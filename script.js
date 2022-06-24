b537fa6343b97aeeb08aae3aa68cbf5f2466c5d0
CDwee <96454637+CDwee@users.noreply.github.com>
2022-06-24 18:24:52 -0500

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,296 @@
+// Started at 10:46 6-24-2022
+
+const express = require("express");
+require("./db");
+const userRouter = require("./routes/user");
+
+const app = express();
+app.use(express.json());
+app.use("/api/user", userRouter);
+
+app.post(
+  "/sign-in",
+  (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.json({ error: "email/password missing!" });
+    next();
+  },
+  (req, res) => {
+    res.send("<h1>Hello I am about page</h1>");
+  }
+);
+
+app.listen(8000, () => {
+  console.log("the port is listening on port 8000");
+});
+
+const express = require("express");
+const {
+  create,
+  verifyEmail,
+  resendEmailVerificationToken,
+} = require("../controllers/user");
+const { userValidtor, validate } = require("../middlewares/validator");
+
+const router = express.Router();
+
+router.post("/create", userValidtor, validate, create);
+router.post("/verify-email", verifyEmail);
+router.post("/resend-email-verification-token", resendEmailVerificationToken);
+
+module.exports = router;
+
+const User = require("../models/user");
+const EmailVerificationToken = require("../models/emailVerificationToken");
+const { isValidObjectId } = require("mongoose");
+const { generateOTP, generateMailTransporter } = require("../utils/mail");
+const { sendError } = require("../utils/helper");
+
+exports.create = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const oldUser = await User.findOne({ email });
+
+  if (oldUser) return sendError(res, "This email is already in use!");
+
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  // generate 6 digit otp
+  let OTP = generateOTP();
+
+  // store otp inside our db
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: newUser._id,
+    token: OTP,
+  });
+
+  await newEmailVerificationToken.save();
+
+  // send that otp to our user
+
+  var transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "verification@reviewapp.com",
+    to: newUser.email,
+    subject: "Email Verification",
+    html: `
+      <p>Your verification OTP</p>
+      <h1>${OTP}</h1>
+
+    `,
+  });
+
+  res.status(201).json({
+    message:
+      "Please verify your email. OTP has been sent to your email accont!",
+  });
+};
+
+exports.verifyEmail = async (req, res) => {
+  const { userId, OTP } = req.body;
+
+  if (!isValidObjectId(userId)) return res.json({ error: "Invalid user!" });
+
+  const user = await User.findById(userId);
+  if (!user) return sendError(res, "user not found!", 404);
+
+  if (user.isVerified) return sendError(res, "user is already verified!");
+
+  const token = await EmailVerificationToken.findOne({ owner: userId });
+  if (!token) return sendError(res, "token not found!");
+
+  const isMatched = await token.compaireToken(OTP);
+  if (!isMatched) return sendError(res, "Please submit a valid OTP!");
+
+  user.isVerified = true;
+  await user.save();
+
+  await EmailVerificationToken.findByIdAndDelete(token._id);
+
+  var transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "verification@reviewapp.com",
+    to: user.email,
+    subject: "Welcome Email",
+    html: "<h1>Welcome to our app and thanks for choosing us.</h1>",
+  });
+  res.json({ message: "Your email is verified." });
+};
+
+exports.resendEmailVerificationToken = async (req, res) => {
+  const { userId } = req.body;
+
+  const user = await User.findById(userId);
+  if (!user) return sendError(res, "user not found!");
+
+  if (user.isVerified)
+    return sendError(res, "This email id is already verified!");
+
+  const alreadyHasToken = await EmailVerificationToken.findOne({
+    owner: userId,
+  });
+  if (alreadyHasToken)
+    return sendError(
+      res,
+      "Only after one hour you can request for another token!"
+    );
+
+  // generate 6 digit otp
+  let OTP = generateOTP();
+
+  // store otp inside our db
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: user._id,
+    token: OTP,
+  });
+
+  await newEmailVerificationToken.save();
+
+  // send that otp to our user
+
+  var transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "verification@reviewapp.com",
+    to: user.email,
+    subject: "Email Verification",
+    html: `
+      <p>Your verification OTP</p>
+      <h1>${OTP}</h1>
+
+    `,
+  });
+
+  res.json({
+    message: "New OTP has been sent to your registered email accout.",
+  });
+};
+
+exports.sendError = (res, error, statusCode = 401) => {
+  res.status(statusCode).json({ error });
+};
+
+const nodemailer = require("nodemailer");
+
+exports.generateOTP = (otp_length = 6) => {
+  let OTP = "";
+  for (let i = 1; i <= otp_length; i++) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+
+  return OTP;
+};
+
+exports.generateMailTransporter = () =>
+  nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "6d011a69a39319",
+      pass: "aa003480e0370e",
+    },
+  });
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const emailVerificationTokenSchema = mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    expires: 3600,
+    default: Date.now(),
+  },
+});
+
+emailVerificationTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    this.token = await bcrypt.hash(this.token, 10);
+  }
+
+  next();
+});
+
+emailVerificationTokenSchema.methods.compaireToken = async function (token) {
+  const result = await bcrypt.compare(token, this.token);
+  return result;
+};
+
+module.exports = mongoose.model(
+  "EmailVerificationToken",
+  emailVerificationTokenSchema
+);
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
+
+const { check, validationResult } = require("express-validator");
+
+exports.userValidtor = [
+  check("name").trim().not().isEmpty().withMessage("Name is missing!"),
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+  check("password")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Password is missing!")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must be 8 to 20 characters long!"),
+];
+
+exports.validate = (req, res, next) => {
+  const error = validationResult(req).array();
+  if (error.length) {
+    return res.json({ error: error[0].msg });
+  }
+
+  next();
+};
+
+// Ended at 6:24 6-24-2022
